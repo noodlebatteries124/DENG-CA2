89514c2675cf3dd43f0a60f34a105571948fde98
haoern <haoerng.25@ichat.sp.edu.sg>
2026-07-21 12:40:04 +0800

    trial

--- DW_insert.sql
+++ DW_insert.sql
@@ -1,1 +1,16 @@
 USE CA2DW2B2407;
+
+INSERT INTO CA2DW2B2407.FactLoan (DateKey, MemberKey, StaffKey, BookCopyKey, LoanDatetime, ReturnDatetime, LoanStatus)
+SELECT
+    d.DateKey,
+    m.MemberKey,
+    s.StaffKey,
+    bc.BookCopyKey,
+    l.LoanDatetime,
+    l.ReturnDatetime,
+    CASE
+        WHEN l.ReturnDatetime IS NULL THEN 'On Loan'
+        WHEN l.ReturnDatetime > l.DueDate THEN 'Returned Late'
+        ELSE 'Returned On Time'
+    END AS LoanStatus
+FROM CA22B2407.Loan l
